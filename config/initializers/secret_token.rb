9988420343eb5110019d001c9b6eb337e92e5a79
Danny Yu <dannyyu92@gmail.com>
2014-04-28 20:50:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scouter9000::Application.config.secret_key_base = 'edb3d0c126e8fad7aff9ee52042174a63733f79823f4604dbdffacca3173938a69a47a96fc445df0157a9a3dac4ed6e095cb046a889babc46fa6ae430a670e15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scouter9000::Application.config.session_store :cookie_store, key: '_scouter9000_session'
