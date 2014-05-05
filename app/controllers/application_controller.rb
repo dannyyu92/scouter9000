ebde95a5fc2469595bfce85bf6903228f821aa54
Danny Yu <dannyyu92@gmail.com>
2014-05-04 21:52:03 -0400

    force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
   acts_as_token_authentication_handler_for User
+  force_ssl
   
   # Apply strong_parameters filtering before CanCan authorization
   # See https://github.com/ryanb/cancan/issues/571#issuecomment-10753675
